977308ca21c606ed19f8f0b0afd78b6b8976ef69
navaneeth.047 <2420030306@klh.edu.in>
2025-10-24 10:49:39 +0530

    Add Week 7 file

---
+++ week 8/src/components/TaskIteam.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+const TaskItem = ({ task, onDelete, onToggle }) => {
+return (
+<li className={task.completed ? "completed" : ""}>
+<span onClick={() => onToggle(task.id)}>{task.title}</span>
+<button onClick={() => onDelete(task.id)}>XX</button>
+</li>
+);
+};
+export default TaskItem;
